46364c02cfcb1b4f55d27bd929d09338bc6296f9
Alex Gergar <alexandra.gergar@gmail.com>
2019-07-07 20:28:58 +0000

    devise simple form edit and style  for log in/out

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'https://flixter-alex-gergar.herokuapp.com/' }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
