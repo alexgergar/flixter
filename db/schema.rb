150df6f915255bbd537654ce6b898e4cefeafefe
Alex Gergar <alexandra.gergar@gmail.com>
2019-07-08 22:57:57 +0000

    L17 Adding Sections to Instructor Course pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_07_220849) do
+ActiveRecord::Schema.define(version: 2019_07_08_213407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 2019_07_07_220849) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_courses_on_user_id"
+  end
+
+  create_table "sections", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
