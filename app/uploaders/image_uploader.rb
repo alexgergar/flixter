f315b14b390bb322a75358da88ebf19fd9fb448f
Alex Gergar <alexandra.gergar@gmail.com>
2019-07-13 23:34:38 +0000

    changed the image uploader to be resize and pad

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -22,7 +22,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-   process resize_to_fill: [800, 350]
+   process resize_and_pad: [800, 534]
   #
   # def scale(width, height)
   #   # do something
