46f67f79a3c4b07ebdb1709c62ed2b89bddeb313
Alex Gergar <alexandra.gergar@gmail.com>
2019-07-19 17:38:06 +0000

    L31 Adding Drag and Drop function with AJAX

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,7 +1,8 @@
 class Instructor::LessonsController < ApplicationController
     before_action :authenticate_user!
-    before_action :require_authorized_for_current_section
-    
+    before_action :require_authorized_for_current_section, only: [:new, :create]
+    before_action :require_authorized_for_current_lesson, only: [:update]
+
     def new
         @lesson = Lesson.new
     end
@@ -15,7 +16,22 @@ class Instructor::LessonsController < ApplicationController
          end
     end
 
+    def update
+        current_lesson.update_attributes(lesson_params)
+        render plain: 'updated!'
+    end
+
     private
+
+    def current_lesson 
+        @current_lesson ||= Lesson.find(params[:id])
+    end
+
+    def require_authorized_for_current_lesson
+        if current_lesson.section.course.user != current_user
+            return render plain: "Unauthorized", status: :unauthorized
+        end
+    end
 
     def require_authorized_for_current_section
         if current_section.course.user != current_user
@@ -29,6 +45,6 @@ class Instructor::LessonsController < ApplicationController
     end
 
     def lesson_params
-      params.require(:lesson).permit(:title, :subtitle, :video)
+      params.require(:lesson).permit(:title, :subtitle, :video, :row_order_position)
     end
 end
