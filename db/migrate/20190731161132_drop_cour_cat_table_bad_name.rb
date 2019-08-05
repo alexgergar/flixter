fa5267c8bc005c30b369c15552a5285c57d146f8
Alex Gergar <alexandra.gergar@gmail.com>
2019-08-05 19:33:05 +0000

    deletion of migration drop table cat course bc of a heroku error

--- db/migrate/20190731161132_drop_cour_cat_table_bad_name.rb
+++
@@ -1,5 +0,0 @@
-class DropCourCatTableBadName < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :categories_courses
-  end
-end
--- db/migrate/20190801170515_drop_category_cat_course_tables.rb
+++
@@ -1,6 +0,0 @@
-class DropCategoryCatCourseTables < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :categories
-    drop_table :categories_courses
-  end
-end
